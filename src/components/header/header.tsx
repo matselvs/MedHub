2fcb736ff76025dd6ad7fdd8e4d97e4c7a4bd7be
Selvs <matheussilva30@gmail.com>
2023-10-20 18:27:58 -0300

    quinto commit

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
-import './Header.css'
+import './header.css'
 
 interface User {
   name: string
